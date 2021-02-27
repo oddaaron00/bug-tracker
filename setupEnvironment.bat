ff1a95915430a133ef0519885af4db9e79aa846a
oddaaron00 <caseygoodings@outlook.com>
2021-02-27 11:33:33 +0000

    Changed setupEnvironment path

--- setupEnvironment.bat
+++ setupEnvironment.bat
@@ -1,2 +1,2 @@
-START cmd /k "cd D:\bug-tracker-test/api & npm start"
-START cmd /k "cd D:\bug-tracker-test/client & npm start"
+START cmd /k "cd C:\Users\casey\Documents\GitHub\bug-tracker\api & npm start"
+START cmd /k "cd C:\Users\casey\Documents\GitHub\bug-tracker\client & npm start"
