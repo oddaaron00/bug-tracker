467c00998bdfe30a448c2a95a559404aced2fb42
oddaaron00 <caseygoodings@outlook.com>
2021-02-27 12:04:50 +0000

    Update login.js

--- api/routes/login.js
+++ api/routes/login.js
@@ -5,27 +5,37 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+//MongoDB details
 const URI = process.env.URI;
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }
 
-router.post('/', function(req, res, next) {
+/**
+ * POST /login
+ * @param {object} res.body
+ * @param {string} res.body.username user's username
+ * @param {string} res.body.password user's unencrypted password
+ */
+router.post('/', (req, res) => {
     mongoose.connect(URI, options)
+      //Find user by username
       User.findOne({ username: req.body.username })
       .then(user => {
           if (user === null) {
+            //If user doesn't exist, send 404
             res.status(404).send(JSON.stringify('No user found.'));
           }
           else {
-            var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+            //If user exists, compare user's encrypted password with encrypted password in database
+            const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+            //If passwords don't match, send 401 with no token
             if (!passwordIsValid) return res.status(401).send(JSON.stringify({ auth: false, token: null }));
-            var token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
-                expiresIn: 3600
-              });
-              
-              res.status(200).send(JSON.stringify({ auth: true, token: token }));
+            //If passwords match, sign token and send 200 with token
+            const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, { expiresIn: 3600 });
+            
+            res.status(200).send(JSON.stringify({ auth: true, token: token }));
           }
       })
       .catch(err => {
@@ -33,7 +43,5 @@ router.post('/', function(req, res, next) {
         res.status(500).send(err);
     });
 });
-
-
 
 module.exports = router;
