2027e7c7bfbb89df6f658165d02985411c83b312
oddaaron00 <caseygoodings@outlook.com>
2021-03-01 19:46:56 +0000

    Update Item.js

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -46,8 +46,8 @@ export default function Item(props) {
 
     return (
         <tr>
-            <td className='tooltip'><input type='text' className='item' value={item.title} onKeyPress={updateOnEnter} onBlur={updateItem} onChange={e => changeItem({...item, title: e.target.value})} /><span className='tooltiptext'>{item.title}</span></td>
-            <td className='tooltip'><input type='text' className='item' value={item.description} onKeyPress={updateOnEnter} onBlur={updateItem} onChange={e => changeItem({...item, description: e.target.value})} />{item.description && <span className='tooltiptext'>{item.description}</span>}</td>
+            <td className='tooltip'><input type='text' className='item' value={item.title} onKeyPress={updateOnEnter} onChange={e => changeItem({...item, title: e.target.value})} /><span className='tooltiptext'>{item.title}</span></td>
+            <td className='tooltip'><input type='text' className='item' value={item.description} onKeyPress={updateOnEnter} onChange={e => changeItem({...item, description: e.target.value})} />{item.description && <span className='tooltiptext'>{item.description}</span>}</td>
             <td>{new Date(item.creation_date).toLocaleString()}</td>
             <td><input type='text' className='item' name='itemDueDate' value={item.due_date ? new Date(item.due_date).toLocaleString() : ''} onChange={e => changeItem({...item, due_date: e.target.value})}/></td>
             <td>
