a9b6c8864c51c52083b6c5a3c5c28d018d858833
oddaaron00 <caseygoodings@outlook.com>
2021-03-03 13:59:30 +0000

    Installed react-router-dom in the correct folder this time lol

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Entry from './components/Entry';
 import Main from './components/Main';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 export default function App() {
   const [isLoggedIn, changeLoginState] = useState(false);
